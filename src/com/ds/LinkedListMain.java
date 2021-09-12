87d2e8188f79929204e7d80695791e0251a4248f
jayesh200026 <jayeshkumar8660@gmail.com>
2021-09-12 22:26:09 +0530

    solved linked list all usecases

---
+++ src/com/ds/LinkedListMain.java
@@ -0,0 +1,26 @@
+package com.ds;
+
+public class LinkedListMain {
+
+	public static void main(String[] args) {
+		LinkedList linkedlist = new LinkedList();
+		linkedlist.add(56);
+		linkedlist.add(30);
+		linkedlist.add(72);
+		//linkedlist.search(30);
+		linkedlist.addAfter(30,40);
+		linkedlist.deleteNode(30);
+		//linkedlist.deleteLast();
+		//linkedlist.inBetween(20, 4);
+		linkedlist.print();
+		
+		OrderedLinkedList orderedList = new OrderedLinkedList();
+		orderedList.add(56);
+		orderedList.add(30);
+		orderedList.add(40);
+		orderedList.add(72);
+		System.out.println("ordered list is");
+		orderedList.print();
+	}
+
+}
